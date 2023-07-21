349dc35e2b55fa5dbc6e7bab04b636d56d6064ea
EthanPark93 <katsube01@gmail.com>
2023-07-21 11:02:52 +0900

    daily_commit

--- programmers/src/qrcode.java
+++ programmers/src/qrcode.java
@@ -1,5 +1,8 @@
 // 풀이가 필요 없는 문제. 문제 요구사항대로 q로 나눈 나머지가 r인 곳의 인덱스에서 정답 문자열에 이어붙인다.
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class qrcode {
     public String solution(int q, int r, String code) {
         String answer = "";
@@ -9,3 +12,15 @@ public class qrcode {
         return answer;
     }
 }
+
+
+// 자바 8 문법을 이용할 수 있지만, 오히려 가독성도 떨어지고 성능적으로 뛰어나지도 않다.
+
+//public class qrcode {
+//    public String solution(int q, int r, String code) {
+//        return IntStream.range(0, code.length())
+//                .filter(operand -> operand % q == r)
+//                .mapToObj(i -> String.valueOf(code.charAt(i)))
+//                .collect(Collectors.joining());
+//    }
+//}
---
+++ programmers/src/배열만들기1.java
@@ -0,0 +1,29 @@
+// 크게 두 가지 방법으로 생각할 수 있다.
+// 첫 번째 방법은 자바8 stream을 이용한 방법으로, 메서드를 통해 순회하면서 조건에 따라 filter해준다.
+// 두 번째 방법은 별다른 메서들르 이용하지 않지만, 주어진 조건을 활용해 보다 성능적으로 최적화된다고 생각한다.
+
+// 자바 int 타입의 성질을 이용하여 특정 배수가 몇개 존재할지 확인하여 배열 크기를 딱 맞게 생성한다.
+// 이후, 그 갯수만큼 반복문을 통해 배수의 갯수를 배열에 추가한다.
+
+// 이 문제에서 배울 점은, 고급 문법이나 짧은 코드가 무조건 좋은 것이 아니라는 것이다.
+// 보다 단순하고 이해하기 쉬운 코드, 그리고 성능적으로도 최적화되는 코드가 좋다는 것을 배웠다.
+
+import java.util.stream.IntStream;
+
+public class 배열만들기1 {
+    public int[] solution(int n, int k) {
+        return IntStream.rangeClosed(1, n).filter(i -> i % k == 0).toArray();
+    }
+}
+
+//public class 배열만들기1 {
+//    public int[] solution(int n, int k) {
+//        int count = n / k;
+//
+//        int[] answer = new int[count];
+//
+//        for (int i = 1; i <= count; i++) answer[i - 1] = k * i;
+//
+//        return answer;
+//    }
+//}
