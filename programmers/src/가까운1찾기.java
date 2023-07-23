2f17cb60ae3fac5383c761bd6fdfdb31020d5dc8
EthanPark93 <katsube01@gmail.com>
2023-07-23 11:00:28 +0900

    daily_commit

---
+++ programmers/src/가까운1찾기.java
@@ -0,0 +1,22 @@
+// 조건이 간단한 문제지만, 풀이의 효율성을 생각하면 핵심 아이디어가 있다.
+// 조건 중에 idx 이상이어야 한다는 것을 생각하면, 루프를 처음부터 돌리면서 if문에 조건 두가지를 넣는 것이 아니라
+// 애초에 idx번째부터 루프를 돌리면 된다.
+
+// stream 문법 연습을 위해 이 방법도 첨부하지만 성능을 생각하면 기본 for문을 사용하는 것이 조금 더 유리하다고 생각한다.
+// 가독성 측면에서도 그렇고, 루프를 idx부터 끝까지 돌려서 filter를 한 stream객체에서 첫번째를 굳이 찾아 반환하는 수고보다,
+// 기본 for문에서 주어진 배열값을 순회하다가 조건에 맞을 때, 바로 루프를 종료시키는 것이 유리하다고 생각한다. ( 배열 길이가 아주 아주 아주 길다고 가정해보자.)
+
+import java.util.stream.IntStream;
+
+class 가까운1찾기 {
+    public int solution(int[] arr, int idx) {
+        return IntStream.range(idx, arr.length).filter(i -> arr[i] == 1).findFirst().orElse(-1);
+    }
+}
+
+//public class 가까운1찾기 {
+//    public int solution(int[] arr, int idx) {
+//        for (int i = idx; i < arr.length; i++) if (arr[i] == 1) return i;
+//        return -1;
+//    }
+//}
---
+++ programmers/src/카운트다운.java
@@ -0,0 +1,31 @@
+// 기본 for문으로 간단하게 풀이할 수 있는 문제.
+// 배열을 들어갈 숫자만큼의 크기로 생성하고 그 크기만큼 순회한다.
+// 배열의 인덱스에 해당하는 위치에 시작 숫자 - 인덱스를 해주면 감소하는 수를 담을 수 있다.
+
+// stream 문법을 활용하면 조금 더 간결하게 적을 수 있다.
+// 이 풀이를 사용하려면 start와 end에 모두 음수 처리를 해주어 배열에 넣고 싶은 순서대로 표현할수 있다.
+// 대신 map할 때 음수처리를 복구해야한다.
+// 음수처리를 하지않고 배열에 넣는 경우 역순 정렬을 위해 코드가 지저분해진다.
+
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public class 카운트다운 {
+    public int[] solution(int start, int end) {
+        return IntStream.rangeClosed(-start, -end).map(i -> -i).toArray();
+//        return IntStream.rangeClosed(end, start).boxed()
+//                .sorted(Comparator.reverseOrder())
+//                .mapToInt(Integer::intValue)
+//                .toArray();
+    }
+}
+
+//public class 카운트다운 {
+//    public int[] solution(int start, int end) {
+//        int[] answer = new int[start-end+1];
+//
+//        for(int i = 0; i < answer.length; i++) answer[i] = start - i;
+//
+//        return answer;
+//    }
+//}
