ae191a2281bb64a3348d7c89095e0c8de1eba850
EthanPark93 <katsube01@gmail.com>
2023-08-03 17:29:43 +0900

    daily_commit

---
+++ programmers/src/홀수vs짝수.java
@@ -0,0 +1,12 @@
+// 짝수 번째와 홀수 번째의 합을 루프를 통해 조건부로 합하고, 그 중 큰 값을 삼항연산자로 리턴한다.
+
+public class 홀수vs짝수 {
+    public int solution(int[] num_list) {
+        int odd = 0, even = 0;
+        for (int i = 0; i < num_list.length; i++) {
+            if (i % 2 == 0) odd += num_list[i];
+            else  even += num_list[i];
+        }
+        return odd > even ? odd : even;
+    }
+}
